22d87b6dde22eb0fc506f796a6b7228cec8c9e11
Voyastic <ameneyro@voyastic.com>
2014-08-05 11:24:26 -0500

    Login php

--- login.php
+++ login.php
@@ -33,24 +33,3 @@ $(function(){
 });
 </script>
 	<!--LOGIN-->
-
-	<div id="men">
-			<div class="container">
-			<div class="three columns">
-			<a href="index.php"><img src="images/logo_03.png"></a></div>
-<ul id="menu">
-    <a href="nosotros.php"><li>Nosotros</li></a>
-    <a href="servicios.php"><li>Servicios</li></a>
-    <a href="premium.php"><li>Premium</li></a>
-    <a href=""><li>Blog</li></a>
-    <a href="contacto.php"><li>Contacto</li></a>
-    <a href="#loginmodal" class="flatbtn" id="modaltrigger"><li>Login</li></a>
-    <a href="#" class="togglebtn"><li>COTIZAR MI VIAJE</li></a>
-
-</ul>
-<script type="text/javascript">
-$(document).ready(function(){
-  $('#menu').slicknav();
-});
-</script>
-</div></div></div>
